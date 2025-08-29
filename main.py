442f6043a550e1c26247fad04a0a5ad0880ef743
Christian Gerard <christian.gerard@binus.ac.id>
2025-08-29 14:20:36 +0700

    changing to py

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-#include <stdio.h>
-
-int main(){
-    printf("Christian Gerard / 29025663345\n");
-    printf("My Favorite Programming language are C and Python");
-}
+print("Christian Gerard / 29025663345\n")
+print("My Favorite Programming language are C and Python")
